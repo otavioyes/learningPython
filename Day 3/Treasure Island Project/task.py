6fd44e3d43b030990977a5807109ffa0ec20cfe9
Otavio Correa <correaotavio0409@gmail.com>
2025-03-24 20:48:37 -0300

    Final Project with .lower()

--- Day 3/Treasure Island Project/task.py
+++ Day 3/Treasure Island Project/task.py
@@ -23,20 +23,20 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
 
-choice1 = input('You are at a crossroads, deciding which way to go "left" or "right":  ')
+choice1 = input('You are at a crossroads, deciding which way to go "left" or "right":  ').lower()
 
 if choice1 == "left":
-    choice2 = input('You are a lost in the forest, do you "make a fire" '
-                    'or "keep walking"?: ')
+    choice2 = input('You\'re a lost in the forest, do you "make a fire" '
+                    'or "keep walking"?: ').lower()
 
     if choice2 == "make a fire":
         choice3 = input('Good choice, it is morning and the beasts have gone to rest,'
-        'do you "build a hut" or look for a "river"?: ')
+        'do you "build a hut" or look for a "river"?: ').lower()
 
         if choice3 == "river":
-            choice4 = input('You re almost there! You ve found a cave with bats and a\n '
+            choice4 = input('You\'re almost there! You\'ve found a cave with bats and a\n '
                             'waterfall with crystal clear water. Do you believe the treasure is\n '
-                            'in the "cave" or inside the "waterfall"?: ')
+                            'in the "cave" or inside the "waterfall"?: ').lower()
             if choice4 == "cave":
                 print("YOU WON! CONGRATULATIONS!")
             else:
