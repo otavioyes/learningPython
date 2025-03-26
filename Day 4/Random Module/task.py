7d38e63fa93c29352dc117c24b09c7f250b78b09
Otavio Correa <correaotavio0409@gmail.com>
2025-03-26 20:59:01 -0300

    Add task.py DONE

---
+++ Day 4/Random Module/task.py
@@ -0,0 +1,15 @@
+import random
+
+# ran_num = random.randint(1, 10)
+# print(ran_num)
+
+# random_number_0_to_1 = random.random() * 10
+# print(random_number_0_to_1)
+
+# random_float = random.uniform(10, 11)
+# print(random_float)
+
+heads_or_tails = random.randint(0,1)
+if heads_or_tails == 0:
+    print("Heads")
+else: print("Tails")
