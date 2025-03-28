b3f03252fd7d3c4764de1cc8b1914f4acbc98823
Otavio Correa <correaotavio0409@gmail.com>
2025-03-28 16:17:26 -0300

    Options to generate random names with list

--- Day 4/Banker Roulette/task.py
+++ Day 4/Banker Roulette/task.py
@@ -2,7 +2,10 @@ import random
 
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
 
-#preciso criar uma variavel que armazena o random.choice e dentro dele vai minha lista com os nomes
+#1st Option
 names = random.choice(friends)
-
 print(f"The {names} pay the bill")
+
+#2nd Option
+random_index = random.randint(0, 4)
+print(friends[random_index])
