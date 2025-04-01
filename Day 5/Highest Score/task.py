833a2bd6d1dbd8f2ee9893fdbfbeeaee9d30e646
Otavio Correa <correaotavio0409@gmail.com>
2025-04-01 19:44:58 -0300

    Error

--- Day 5/Highest Score/task.py
+++ Day 5/Highest Score/task.py
@@ -1,2 +1,16 @@
+from sys import getsizeof
+
 student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
 print(range(1, 10))
+
+#print(len(bytes(student_scores))) # 19
+#print(getsizeof(student_scores)) # 216
+
+#print(max(student_scores)) #199
+#print(min(student_scores)) #24
+
+nota = 0
+for score in student_scores:
+    if nota > score:
+        nota = score
+    print(score)
