d9dc363eff1665d940b45e28c1375e9f7e534ba4
Otavio Correa <correaotavio0409@gmail.com>
2025-04-02 15:25:53 -0300

    Password Generator

--- Day 5/Password Generator Project/task.py
+++ Day 5/Password Generator Project/task.py
@@ -1,3 +1,5 @@
+import random
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -7,4 +9,34 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+#EASY 
+# password = ''
+# for i in range(0, nr_letters):
+#     password += random.choice(letters)
+#
+# for i in range(0, nr_symbols):
+#     password += random.choice(symbols)
+#
+# for i in range(0, nr_numbers):
+#     password += random.choice(numbers)
+# print(password)
 
+password_list = [] #variavel que recebe uma lista
+for i in range(0, nr_letters): # intervalo que eu quero
+    password_list.append(random.choice(letters)) # Uso append e dentro coloco random.choice e dentro a list/var que eu quero
+
+for i in range(0, nr_symbols):
+    password_list.append(random.choice(symbols))
+
+for i in range(0, nr_numbers):
+    password_list.append(random.choice(numbers))
+
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
+
+password = ""
+for i in password_list:
+    password += i
+
+print(f"Your password: {password}")
