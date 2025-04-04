6223dc3649c27acaf22fc73c422d0d34ff66af6a
Otavio Correa <correaotavio0409@gmail.com>
2025-04-04 20:32:25 -0300

    Test FUnctions

--- Day 6/Functions/task.py
+++ Day 6/Functions/task.py
@@ -1,11 +1,25 @@
-password_pass = input("New password: ")
+# password_pass = input("New password: ")
+#
+# def password():
+#     while True:
+#         new_password = input("Enter your password: ")
+#         if password_pass == new_password:
+#             print("Welcome")
+#         else:
+#             print("Acess invalid")
+#
+# password()
 
-def password():
-    while True:
-        new_password = input("Enter your password: ")
-        if password_pass == new_password:
-            print("Welcome")
+def while_check():
+    total = 0
+    while total <= 10000:
+        print(f"TOTAL = {total}")
+        if total == 10000:
+            print(f"o total é : {total}")
         else:
-            print("Acess invalid")
+            print()
+        total += (1 * 3.1415) / 2
 
-password()
+
+
+while_check()
