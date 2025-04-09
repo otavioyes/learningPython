906b307712c8b5763f648109d0cf4b56c8aa576e
Otavio Correa <correaotavio0409@gmail.com>
2025-04-09 15:46:48 -0300

    Add while loop

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -4,18 +4,39 @@ word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
-# TODO-1: Create a "placeholder" with the same number of blanks as the chosen_word
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
+print(placeholder)
 
-guess = input("Guess a letter: ").lower()
+# TODO-1: - Use a while loop to let the user guess again.
+game_over = False
+correct_letter = []
 
-placeholder_str = " ".join(["_" for _ in chosen_word])
-print(placeholder_str)
+display = ""
 
-# TODO-2: Create a "display" that puts the guess letter in the right positions and _ in the rest of the string.
-display = ["_" for _ in chosen_word]
-for index, letter in enumerate(chosen_word):
-    if letter == guess:
-        display[index] = letter
-    else:
-        print(" ".join(display))
+#enquanto não for game over
+while not game_over:
+    #imprima
+    guess = input("Guess a letter: ").lower()
+
+#para cada letra em chosen_word
+    for letter in chosen_word:
+    
+        if letter == guess:
+            display += letter
+            correct_letter.append(guess)
+        elif letter == guess:
+            display += letter
+        else:
+            display = "_"
+
+# TODO-2: Change the for loop so that you keep the previous correct letters in display.
+
+    print(display)
+
+if "_" not in display:
+    game_over = True
+    print("You win.")
 
